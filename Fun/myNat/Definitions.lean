a7a9006d3499c5fd654dfb453d03ec95576b317d
HannahSantos <hannah.santos.441@ufrn.edu.br>
2024-10-13 11:22:45 -0300

    Added myNat.mul function

--- Fun/myNat/Definitions.lean
+++ Fun/myNat/Definitions.lean
@@ -7,3 +7,8 @@ def add : myNat → myNat → myNat
   | n, .O => n
   | n, (.S m) => .S (add n m)
 infixl:65 " + " => add
+
+def mul : myNat → myNat → myNat
+  | _, .O => .O
+  | n, .S m => (mul n m) + n
+infixl:65 " * " => mul
