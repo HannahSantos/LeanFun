bc4609ab789fdc19f801df6dc8a7bca4284aa4bd
HannahSantos <hannah.santos.441@ufrn.edu.br>
2024-10-13 20:02:40 -0300

    Added Add_succ_L theorem

--- Fun/myNat/Theorems.lean
+++ Fun/myNat/Theorems.lean
@@ -2,6 +2,10 @@ import Fun.myNat.Definitions
 
 open myNat
 
+----------------------------------------------------------
+-- Soma:
+----------------------------------------------------------
+
 theorem Add_zero_L :
   ∀ (n : myNat), O + n = n :=
 by
@@ -9,3 +13,11 @@ by
   induction n with
   | O => rw [add]
   | S k HI => rw [add, HI]
+
+theorem Add_succ_L :
+  ∀ (n m : myNat), (S n) + m = S (n + m) :=
+by
+  intro n m
+  induction m with
+  | O => rw [add, add]
+  | S k HI => rw [add, add, HI]
